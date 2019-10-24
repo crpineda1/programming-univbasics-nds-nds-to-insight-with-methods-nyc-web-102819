d187b9376ed0266f161afbc1dc18bff272eb30f8
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-24 01:35:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,16 +40,6 @@ end
 # using director_data as input
 def gross_for_director(director_data)
 
-  nds = directors_database
-  director = director_data
-  movie = 0
-  total = 0
-  movie_count = nds[director][:movies].count
-  while movie < movie_count do
-    total += nds[director][:movies][movie][:worldwide_gross]
-    movie += 1
-  end
-  return total
 
   p director_data
 end
